42e90be2a9e051bdfd757cb11dfc2af608f5a285
TeerapatTrepopsakulsin <teerapat.tre@ku.th>
2024-03-13 21:58:22 +0700

    Unit Test for Counter

--- counter/test_counter.py
+++ counter/test_counter.py
@@ -7,3 +7,33 @@
 
    You can use pytest or unittest.
 """
+
+
+import unittest
+from counter import Counter
+
+
+class TestCounter(unittest.TestCase):
+
+    def test_count_and_increment(self):
+        counter = Counter()
+        self.assertEqual(0, counter.count)
+        self.assertEqual(0, counter.count)
+        self.assertEqual(1, counter.increment())
+        counter.increment()
+        self.assertEqual(2, counter.count)
+        self.assertEqual(2, counter.count)
+
+    def test_singleton(self):
+        counter = Counter()
+        counter.increment()
+        counter.increment()
+        self.assertEqual(2, counter.count)
+
+        counter2 = Counter()
+        self.assertTrue(counter2 is counter)
+        self.assertEqual(2, counter2.count)
+        self.assertEqual(3, counter2.increment())
+        self.assertEqual(3, counter.count)
+        counter.increment()
+        self.assertEqual(counter.count, counter2.count)
